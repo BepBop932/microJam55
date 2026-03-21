214aa4af490b5bbf1e1df069c7a7eb8ca6a1b9c3
BepBop932 <josee7040@gmail.com>
2026-03-20 21:45:42 -0400

    init

---
+++ scripts/mouseLight.gd
@@ -0,0 +1,7 @@
+extends PointLight2D
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	global_position = get_global_mouse_position()
